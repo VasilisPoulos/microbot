c3d86dfe3b9bf5bcf86658e73961a90cb5757791
VasilisPoulos <cs02805@uoi.gr>
2021-12-10 22:52:56 +0200

    Implemented limiters, found K values. Started to log process.

---
+++ log.md
@@ -0,0 +1,92 @@
+# Activity log
+
+## Questions
+
+- Why would i need /tf_static for always true data if thats not the case in the real world?
+
+## Notes
+
+Turtlebot setup link <https://automaticaddison.com/how-to-launch-the-turtlebot3-simulation-with-ros/>
+
+- **Dec 10:** Completing cotroller: I think i can reduce spikes if i find the right values for K's. 
+  I removed the round-to-stop if, now robot tries to correct it's position after a while. Maybe i 
+  need to re-implement the condition.
+    ![image](../microrobot/images/speed_acceleration_spikes.png)
+
+  **Final**
+
+    ![image](../microrobot/images/speed_final.png)
+
+### ROS commands
+
+Launch the virtual Turtlebot3 using RViz.
+
+```bash
+roslaunch turtlebot3_fake turtlebot3_fake.launch
+```
+
+Gazebo launch
+
+```bash
+roslaunch turtlebot3_gazebo turtlebot3_empty_world.launch
+```
+
+Ploting gui
+
+```bash
+rqt_plot
+```
+
+Topics gui
+
+```bash
+rqt
+```
+
+### Turtlebot3 Spec
+
+Its characteristics and specificities
+<https://www.robot-advance.com/EN/actualite-turtlebot3-burger-by-robotis-149.htm>
+
+- Maximum travel speed: 0.22 m / s.
+- Maximum rotational speed: 2.84 rad / s. (167.72 deg / s.
+- Maximum payload: 15 kg.
+- Size (L x W x H): 138 mm x 178 mm x 192 mm.
+- Maximum threshold height: 10 mm.
+- Total weight (including SBC, battery and sensors): 1 kg.
+- Charging time: 2h30.
+
+## Todo
+
+*sorted by importance*
+
+- [ ] Experiment with constant v_des.
+- [ ] Read more about PID's.
+- [ ] Make a better blender model.
+- [ ] Fix workspace. Add setup.py and __init__.py
+- [ ] Make a launch file.
+
+## Completed
+
+### Dec 10
+
+- [x] Implement a speed limiter, controller.
+- [X] Find right K's.
+- [x] Plot speed acceleration etc.
+- [x] Whats /tf - /tf_static difference.
+  - **Answer:** \
+    *source* :<http://wiki.ros.org/turtlebot3_example>
+    - tf (tf2_msgs/TFMessage)
+      Contains the coordinate transformation such as base_footprint and odom.
+    - tf_static (tf2_msgs/TFMessage)
+      Topic has same format as "/tf", however it is expected that any transform on this topic can be considered true for all time.  
+    **Result:** \
+    /tf is noisy, /tf_static has nothing to post.
+
+    ![image](../microrobot/images/tf_noise_1.png)
+
+    ![image](../microrobot/images/tf_noise.png)
+
+### Dec 8
+
+- [x] Convert trajectory node to a service.
--- src/controller.py
+++ src/controller.py
@@ -7,11 +7,28 @@ from geometry_msgs.msg import Twist
 from nav_msgs.msg import Odometry
 from microrobot.srv import *
 from tf.transformations import euler_from_quaternion
+from matplotlib import pyplot as plt
 
-global _roll, _pitch, _yaw, _x, _y, _theta, _trajectory, _direction
+global _roll, _pitch, _yaw, _x, _y, _theta, _trajectory, _direction, _linear_limit, _Kr, _Ka, \
+    _Kb
+
 _roll = _pitch = _yaw = _x = _y = _theta = 0.0
+_linear_speed_limit = 0.15
+_angular_speed_limit = 1.9
 _trajectory = []
 _direction = []
+_Kr = 0.9 # drive towards b
+_Ka = 0.6 # turn the robot direction to b
+_Kb = 0.3 # turn the robot to its final direction.
+_width = 0.178
+
+def plot_figure(x, y, time):
+    fig, axs = plt.subplots(2, 2)
+    axs[0, 0].plot(time, x)
+    axs[0, 0].set_title('Axis [0,0]')
+    axs[0, 1].plot(time, y)
+    axs[0, 1].set_title('Axis [0,0]')
+    plt.show()
 
 
 def odom_callback(msg):   
@@ -20,15 +37,6 @@ def odom_callback(msg):
     _y = msg.pose.pose.position.y
     rot_q = msg.pose.pose.orientation
     (_roll, _pitch, _theta) = euler_from_quaternion([rot_q.x, rot_q.y, rot_q.z, rot_q.w])
-
-
-# def trajectory_callback(data):
-#     global _trajectory, _direction
-#     converted_trajectory = (np.array(data.trajectory)).reshape(-1, 3).tolist()
-#     _trajectory = converted_trajectory
-#     _direction = data.direction
-#     rospy.loginfo('Got: %s trajectory: %d direction: %d', data.message, len(converted_trajectory), \
-#         len(data.direction))
 
 
 def clothoid_generator_client(trajectory_points):
@@ -47,7 +55,7 @@ def start_controller():
     global _trajectory, _direction
 
     rospy.init_node('controller', anonymous=False)
-    rate = rospy.Rate(100)  # 10hz
+    rate = rospy.Rate(10)  # 10hz
     
     trajectory_points = [
             np.array([1., 1., 1.]), 
@@ -64,50 +72,73 @@ def start_controller():
     twist_msg = Twist()
     
     i = 0
-    x_des = 1#_trajectory[0][0]
-    y_des = 1#_trajectory[0][1]
-    theta_des = np.radians(_direction[0])
-    Kr = 0.07
-    Ka = 0.09
-    Kb = 0.09
-    width = 0.178
+    x_des = 1 #_trajectory[0][0]
+    y_des = 1 #_trajectory[0][1]
+    theta_des = 0 #np.radians(_direction[0])
+    max_liner_speed = 0
+    max_angular_speed = 0
+    time_log = []
+    x_log = []
+    y_log = []
+    theta_log = []
 
     while not rospy.is_shutdown():
         r = np.sqrt(((x_des - _x)**2) + ((y_des - _y)**2))
         alpha = (np.arctan2(y_des - _y, x_des - _x)) - _theta
         beta = _theta + alpha - theta_des
-        v_des = Kr*r
-        omega_des = ((Ka*alpha) + (Kb*beta)) / width
+        v_des = _Kr*r
+        omega_des = ((_Ka*alpha) + (_Kb*beta)) / _width
 
-        twist_msg.linear.x = v_des #m/s
-        twist_msg.linear.y = 0.0
-        twist_msg.linear.z = 0.0
+        # Linear speed limiter.
+        if v_des > _linear_speed_limit:
+            twist_msg.linear.x = _linear_speed_limit
+        else:
+            twist_msg.linear.x = v_des # m/s
 
-        twist_msg.angular.x = 0.0
-        twist_msg.angular.y = 0.0
-        twist_msg.angular.z = omega_des #rad/s
+        # Angular speed limiter.
+        if np.abs(omega_des) > np.abs(_angular_speed_limit):
+            if omega_des > 0:
+                twist_msg.angular.z = _angular_speed_limit
+            else:
+                twist_msg.angular.z = - _angular_speed_limit
+        else:
+            twist_msg.angular.z = omega_des # rad/s
+        
+        # Information Logging.
+        if twist_msg.linear.x > max_liner_speed:
+            max_liner_speed = twist_msg.linear.x
+        
+        if twist_msg.angular.z > max_angular_speed:
+            max_angular_speed = twist_msg.angular.z
 
+        x_log.append(_x)
+        y_log.append(_y)
+        theta_log.append(_theta)
+        time_log.append(rospy.get_time())
 
-        if (round(_x, 1) == x_des and \
-            round(_y, 1) == y_des and \
-            round(_theta, 1) == theta_des):
-            twist_msg.linear.x = 0.0
-            twist_msg.angular.z = 0.0
-            print('HEREHEREHEREHEREHEREHERE')
-            # change x_des, y_des, theta_des\
-            # plot when there speed acceleration etc.
-            if i < len(trajectory_points):
-                x_des = _trajectory[i][0]
-                y_des = _trajectory[i][1]
-                theta_des = np.radians(_direction[i])
-
-        rospy.loginfo('\nx %f y %f theta %f\nlinear x %f, y %f, z %f \nangular x %f,\
-            y %f, z %f\ntarget%d: %f %f th: %f'\
-            , _x, _y, _theta,\
-            twist_msg.linear.x, twist_msg.linear.y, twist_msg.linear.z,\
-            twist_msg.angular.x, twist_msg.angular.y, twist_msg.angular.z,
+        # if (round(_x, 1) == round(x_des, 1) and \
+        #     round(_y, 1) == round(y_des, 1) and \
+        #     round(_theta, 1) == round(theta_des, 1)):
+        #     twist_msg.linear.x = 0.0
+        #     twist_msg.angular.z = 0.0
+            
+        #     rospy.loginfo('\nmax linear: %f, max angular: %f', \
+        #          max_liner_speed, max_angular_speed)
+        #     rospy.signal_shutdown('stop')
+        #     exit()
+        #     # change x_des, y_des, theta_des\
+        #     # plot when there speed acceleration etc.
+        #     # if i < len(trajectory_points):
+        #     #     i += 1
+        #     #     x_des = _trajectory[i][0]
+        #     #     y_des = _trajectory[i][1]
+        #     #     theta_des = np.radians(_direction[i])
+        #     # plot_figure(x_log, y_log, time_log)
+        
+        rospy.loginfo('\nx %f y %f theta %f\nlinear x %f' + \
+            '\nangular z %f\ntarget%d: %f %f th: %f'\
+            , _x, _y, _theta, twist_msg.linear.x, twist_msg.angular.z, \
             i, x_des, y_des, theta_des)
-
         pub.publish(twist_msg)
         rate.sleep()
 
