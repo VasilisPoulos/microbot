d2fa8e68433e3a9aea4e210c01050a8dda5c1c10
VasilisPoulos <cs02805@uoi.gr>
2022-09-05 22:32:10 +0300

    add: restarted low level controller

--- src/microrobot/nodes/low_level_controller.py
+++ src/microrobot/nodes/low_level_controller.py
@@ -2,23 +2,25 @@
 
 import rospy
 import math
-from geometry_msgs.msg import Twist 
-from gazebo_msgs.msg import ModelStates
-
+#from geometry_msgs.msg import Twist 
+#from gazebo_msgs.msg import ModelStates
+from nav_msgs.msg import Odometry
+from std_msgs.msg import Float64
+omega_motor = 0
 # def callback(data):
 #     rospy.loginfo(rospy.get_caller_id() + " heard linear %s angular %s", 
 #         data.linear.x, data.angular.z)
 
 def model_speed(msg):
+    global omega_motor
+    motor_bias = 1000
+    K_c = 5000
+    v_desired = 0.005
+    v_current = msg.twist.twist.linear.x
+    
+    omega_motor = K_c * (v_desired - v_current) + motor_bias
 
-    v_x = msg.twist[2].linear.x
-    v_y = msg.twist[2].linear.y
-    x = msg.pose[2].position.x
-    y = msg.pose[2].position.y
-    v = math.sqrt(v_x ** 2 + v_y ** 2)
-
-    rospy.loginfo("x: %f y: %f v: %f v_x: %f v_y: %f", round(x, 7), round(y, 7), round(v, 8),
-        round(v_x, 8), round(v_y, 8))
+    rospy.loginfo("omega: %f, linear_speed: %f", omega_motor, v_current)
 
 if __name__ == '__main__':
     try:
@@ -26,12 +28,16 @@ if __name__ == '__main__':
         rate = rospy.Rate(10)
         rospy.loginfo('started low_level_controller node')
         
-        # cmd_vel = rospy.Subscriber('/microrobot/cmd_vel', Twist, callback)
-        #start_time = rospy.get_time()
-        #rospy.loginfo("listening %s".format(start_time))
+        left_motor_topic = "/microbot/left_joint_velocity_controller/command"
+        right_motor_topic = "/microbot/right_joint_velocity_controller/command"
 
+        left_motor_publisher = rospy.Publisher(left_motor_topic, Float64, queue_size=1)
+        right_motor_publisher = rospy.Publisher(right_motor_topic, Float64, queue_size=1)
+        odometry_subscriber = rospy.Subscriber("/odom", Odometry, model_speed)
+        
         while not rospy.is_shutdown():
-            sub = rospy.Subscriber("/gazebo/link_states", ModelStates, model_speed)
+            left_motor_publisher.publish(omega_motor)
+            right_motor_publisher.publish(-omega_motor)
             rate.sleep()
 
     except rospy.ROSInterruptException:
--- src/microrobot/nodes/test.py
+++ src/microrobot/nodes/test.py
@@ -27,7 +27,7 @@ if __name__ == '__main__':
 
         while not rospy.is_shutdown():
             left_motor_publisher.publish(3500.0)
-            right_motor_publisher.publish(0000.0)
+            right_motor_publisher.publish(-3500.0)
             rate.sleep()
 
     except rospy.ROSInterruptException:
