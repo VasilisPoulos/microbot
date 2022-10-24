db4891addc82eb3485a644b94d5c724bc9f4fc49
VasilisPoulos <cs02805@uoi.gr>
2022-10-24 20:18:27 +0300

    testing - tuning low level pid controller

--- src/microrobot/nodes/test.py
+++ src/microrobot/nodes/test.py
@@ -8,8 +8,9 @@ def reset_speed():
     right_motor_publisher.publish(0.0)
     rospy.loginfo('resetting speed...')
 
-l_rpm = 1000.0
-r_rpm = 1000.0
+l_rpm = 1600.0
+r_rpm = 1600.0
+test_mode = False
 
 if __name__ == '__main__':
     try:
@@ -21,16 +22,23 @@ if __name__ == '__main__':
             Float64, queue_size=1)
         right_motor_publisher = rospy.Publisher( \
             '/microbot/right_joint_velocity_controller/command', \
-            Float64, queue_size=1)
+            Float64, queue_size=1)        
 
+        left_set_point_publisher = rospy.Publisher( \
+            '/left_set_point', \
+            Float64, queue_size=1)    
         
-
         rospy.on_shutdown(reset_speed)
 
         while not rospy.is_shutdown():
-            l_rpm = float(input("new_rpm:"))
-            r_rpm = l_rpm
+
+            if test_mode:
+                l_rpm = float(input("new_rpm:"))
+                r_rpm = l_rpm
+
             rospy.loginfo('l_rpm: %.1f r_rpm: %.1f', l_rpm, r_rpm)
+
+            left_set_point_publisher.publish(l_rpm)
             left_motor_publisher.publish(l_rpm)
             right_motor_publisher.publish(r_rpm)
             rate.sleep()
